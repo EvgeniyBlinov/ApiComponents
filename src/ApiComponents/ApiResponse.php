2e45b5d69d1eac59c61bc0e04bfd111c6138a4de
Blinov Evgeniy <eblinov@alvioneurope.ru>
2015-03-06 17:38:47 +0300

    master Work stage 2015-03-06 17:38:47 A  src/ApiComponents/ApiResponse.php A  src/autoload.php

---
+++ src/ApiComponents/ApiResponse.php
@@ -0,0 +1,76 @@
+<?php
+namespace ApiComponents;
+
+/**
+ * ApiResponse class
+ *
+ **/
+class ApiResponse
+{
+    /**
+     * @var array of data
+     **/
+    public $data   = array();
+
+    /**
+     * @var array of meta data
+     **/
+    public $meta   = array();
+
+    /**
+     *@var integer of response status
+     **/
+    public $status = 200;
+
+    /**
+     * Constructor
+     *
+     * @param mixed $response
+     **/
+    function __construct($data, array $options)
+    {
+        $this->data = (array) $data;
+        $this->setOptions($options);
+    }
+
+    /**
+     * Set options
+     *
+     * @param array $options
+     * @return ApiResponse
+     **/
+    public function setOptions(array $options)
+    {
+        foreach ($options as $key => $value) {
+            if (property_exists(get_class($this), $key)) {
+                $this->$key = $value;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Convert response to array
+     *
+     * @return array
+     **/
+    public function __toArray()
+    {
+        return array(
+            'status' => $this->status,
+            'meta'   => $this->meta,
+            'data'   => $this->data,
+        );
+    }
+
+    /**
+     * Convert response to string
+     *
+     * @return string
+     **/
+    public function __toString()
+    {
+        return json_encode($this->__toArray());
+    }
+}
